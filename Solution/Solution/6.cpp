3a99d7d921401650befe8b40fc714e7b888d8313
int_boy <intboy.1423@gmail.com>
2018-01-16 15:10:49 +0800

    update

--- Solution/Solution/6.cpp
+++ Solution/Solution/6.cpp
@@ -10,11 +10,59 @@ struct BinaryTreeNode
 	BinaryTreeNode *right;
 };
 
-BinaryTreeNode* RebuildBT(vector<int> preorder, int prebegin, int preend, vector<int> inorder, int inbegin, int inend, BinaryTreeNode *root)
+BinaryTreeNode* ConstructCore(int *startPreorder, int *endPreorder, int *startInorder, int *endInorder)
+{
+	// 前序遍历序列的第一个数字是根节点的值
+	int rootValue = startPreorder[0];
+	BinaryTreeNode *root = new BinaryTreeNode;
+	root->m_nValue = rootValue;
+	root->left = root->right = NULL;
+
+	if (startPreorder == endPreorder)
+	// 必要吗？
+	{
+		if (startInorder == endInorder && *startPreorder == *startInorder)
+			return root;
+		else
+			throw std::exception("Invalid input.");
+	}
+
+	// 在中序遍历中找到根节点的值
+	int *rootInorder = startInorder;
+	while (rootInorder < endInorder && rootValue != *rootInorder)
+		// 指针++与比较，个人很少使用
+		++rootInorder;
+
+	if(rootInorder == endInorder && *rootInorder != rootValue)
+		throw std::exception("Invalid input.");
+
+	int leftLength = rootInorder - startInorder;
+	int *leftPreorderEnd = startPreorder + leftLength;
+	if (leftLength > 0)
+		root->left = ConstructCore(startPreorder + 1, leftPreorderEnd, startInorder, rootInorder - 1);
+	if (leftLength < endPreorder - startPreorder)
+	{
+		root->right = ConstructCore(leftPreorderEnd + 1, endPreorder, rootInorder + 1, endInorder);
+	}
+
+	return root;
+}
+
+BinaryTreeNode* Construct(int *preorder, int *inorder, int length)
+{
+	if (preorder == NULL || inorder == NULL || length <= 0)
+		return NULL;
+
+	return ConstructCore(preorder, preorder + length - 1,
+		inorder, inorder + length - 1);
+}
+
+BinaryTreeNode* RebuildBT(vector<int> preorder, int prebegin, int preend, vector<int> inorder, int inbegin, int inend)
 {
 	if (prebegin > preend)
 		return NULL;
-	root = new BinaryTreeNode;
+
+	BinaryTreeNode *root = new BinaryTreeNode;
 	root->m_nValue = preorder[prebegin];
 	root->left = root->right = NULL;
 	int pivot = 0;
@@ -26,9 +74,9 @@ BinaryTreeNode* RebuildBT(vector<int> preorder, int prebegin, int preend, vector
 	{
 		cerr << "input order isn't correct.";
 		exit(-1);
-	}
-	root->left = RebuildBT(preorder, prebegin + 1, prebegin + pivot, inorder, inbegin, inbegin + pivot, root->left);
-	root->right = RebuildBT(preorder, prebegin + pivot + 1, preend, inorder, inbegin + pivot + 2, inend, root->right);
+	} 
+	root->left = RebuildBT(preorder, prebegin + 1, prebegin + pivot, inorder, inbegin, inbegin + pivot - 1);
+	root->right = RebuildBT(preorder, prebegin + pivot + 1, preend, inorder, inbegin + pivot + 1, inend);
 
 	return root;
 }
@@ -42,29 +90,51 @@ void preOrderTraverse(BinaryTreeNode *root)
 	preOrderTraverse(root->right);
 }
 
-int main()
+void inOrderTraverse(BinaryTreeNode *root)
 {
-	int nodenum = 0;
-	cout << "Input node number : ";
-	cin >> nodenum;
-	vector<int> preorder, inorder;
-	cout << "Input PreOder : ";
-	for (int i = 0; i < nodenum; i++)
-	{
-		int tmp;
-		cin >> tmp;
-		preorder.push_back(tmp);
-	}
-	cout << "Input InOder : ";
-	for (int i = 0; i < nodenum; i++)
-	{
-		int tmp;
-		cin >> tmp;
-		inorder.push_back(tmp);
-	}
-	BinaryTreeNode *root = NULL;
-	root = RebuildBT(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, root);
-	preOrderTraverse(root);
-
-	return 0;
+	if (root == NULL)
+		return;
+	
+	inOrderTraverse(root->left);
+	cout << root->m_nValue << endl;
+	inOrderTraverse(root->right);
 }
+
+void postOrderTraverse(BinaryTreeNode *root)
+{
+	if (root == NULL)
+		return;
+
+	postOrderTraverse(root->left);
+	postOrderTraverse(root->right);
+	cout << root->m_nValue << endl;
+}
+
+//int main()
+//{
+//	int nodenum = 0;
+//	cout << "Input node number : ";
+//	cin >> nodenum;
+//	vector<int> preorder, inorder;
+//	cout << "Input PreOder : ";
+//	for (int i = 0; i < nodenum; i++)
+//	{
+//		int tmp;
+//		cin >> tmp;
+//		preorder.push_back(tmp);
+//	}
+//	cout << "Input InOder : ";
+//	for (int i = 0; i < nodenum; i++)
+//	{
+//		int tmp;
+//		cin >> tmp;
+//		inorder.push_back(tmp);
+//	}
+//	BinaryTreeNode *root = NULL;
+//	root = RebuildBT(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+//	preOrderTraverse(root);
+//	inOrderTraverse(root);
+//	postOrderTraverse(root);
+//
+//	return 0;
+//}
