b32b97cfcbc4d0fc4d5f4755048a0d753d931926
int_boy <intboy.1423@gmail.com>
2018-01-18 22:52:08 +0800

    update

---
+++ Solution/Solution/13.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode
+{
+	int m_nValue;
+	ListNode *m_pNext;
+};
+
+void DeleteNode(ListNode **pListHead, ListNode *pToBeDeleted)
+{
+	if (pListHead == NULL || *pListHead == NULL || pToBeDeleted == NULL)
+		return;
+
+	if (*pListHead == pToBeDeleted && pToBeDeleted->m_pNext == NULL)
+	// 只有一个节点的情况
+	{
+		delete pToBeDeleted;
+		pToBeDeleted = NULL;
+		*pListHead = NULL;
+		return;
+	}
+
+	// 先考虑删除最后一个节点的情况
+	if (pToBeDeleted->m_pNext == NULL)
+	{
+		ListNode *pPreNode = *pListHead;
+		while (pPreNode->m_pNext != pToBeDeleted)
+			pPreNode = pPreNode->m_pNext;
+		pPreNode->m_pNext = NULL;
+		delete pToBeDeleted;
+		pToBeDeleted = NULL;
+	}
+	else
+	{
+		// swap and delete
+		ListNode *ptmp = pToBeDeleted->m_pNext;
+		pToBeDeleted->m_nValue = ptmp->m_nValue;
+		pToBeDeleted->m_pNext = ptmp->m_pNext;
+		delete ptmp;
+		ptmp = NULL;
+	}
+
+	
+}
+
+void PrintList(ListNode **pListHead)
+{
+	if (pListHead == NULL)
+		return;
+	ListNode *ptmpNode = *pListHead;
+	while (ptmpNode)
+	{
+		cout << ptmpNode->m_nValue << '\t';
+		ptmpNode = ptmpNode->m_pNext;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int n;
+	cout << "Input node num : ";
+	cin >> n;
+
+	ListNode **pListHead = NULL, *pHeadNode = NULL, *pcurNode = NULL;
+	vector<ListNode *> pListNodes;
+	
+	bool isFirst = true;
+	while (n--)
+	{
+		ListNode *ptmpNode = new ListNode;
+		cin >> ptmpNode->m_nValue;
+		ptmpNode->m_pNext = NULL;
+		if (isFirst)
+		{
+			pHeadNode = ptmpNode;
+			pListHead = &pHeadNode;
+			pcurNode = ptmpNode;
+			isFirst = false;
+		}
+		else
+		{
+			pcurNode->m_pNext = ptmpNode;
+			pcurNode = ptmpNode;
+		}
+		pListNodes.push_back(pcurNode);
+	}
+
+	PrintList(pListHead);
+
+
+	while (true)
+	{
+		int deleteN;
+		cout << "Input element order you wanna delete : ";
+		cin >> deleteN;
+		DeleteNode(pListHead, pListNodes[deleteN]);
+		PrintList(pListHead);
+	}
+
+
+	return 0;
+}
