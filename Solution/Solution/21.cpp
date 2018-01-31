d525627e967b9446970e2043296b05fd9c313d9c
int_boy <intboy.1423@gmail.com>
2018-01-31 14:29:36 +0800

    update

--- Solution/Solution/21.cpp
+++ Solution/Solution/21.cpp
@@ -1,25 +1,25 @@
 #include "MyStack.h"
 #include <iostream>
-
-int main()
-{
-	MyStack s;
-
-	for (int i = 0; i < 10; i++)
-		s.push(i);
-
-	cout << s.min() << endl;
-
-	s.pop();
-	cout << s.min() << endl;
-	s.push(-1);
-	cout << s.min() << endl;
-	s.pop();
-	cout << s.min() << endl;
-	s.push(-1);
-	cout << s.min() << endl;
-	while (s.pop())
-		;
-	cout << s.min() << endl;
-	return 0;
-}
+//
+//int main()
+//{
+//	MyStack s;
+//
+//	for (int i = 0; i < 10; i++)
+//		s.push(i);
+//
+//	cout << s.min() << endl;
+//
+//	s.pop();
+//	cout << s.min() << endl;
+//	s.push(-1);
+//	cout << s.min() << endl;
+//	s.pop();
+//	cout << s.min() << endl;
+//	s.push(-1);
+//	cout << s.min() << endl;
+//	while (s.pop())
+//		;
+//	cout << s.min() << endl;
+//	return 0;
+//}
--- Solution/Solution/22.cpp
+++ Solution/Solution/22.cpp
@@ -1,9 +1,91 @@
 #include <stack>
+#include <vector>
+#include <set>
+#include <iostream>
 
 using namespace std;
 
-bool isRightOrder(int *push_order, int *pop_order)
+bool isRightOrder(vector<int> &push_order, vector<int> &pop_order)
 {
+	set<int> in_stack;
+	stack<int> simulate_stack;
+	int index_pop, index_push;
+	index_pop = index_push = 0;
+	while (true)
+	{
+		if (index_pop == pop_order.size())
+			return true;
 
+		
+		if (index_push < push_order.size() && push_order[index_push] == pop_order[index_pop])
+		// skip same order elem
+		{
+			++index_pop;
+			++index_push;
+		}
+		else if (in_stack.count(pop_order[index_pop]) > 0)
+		// pop if elem in in_stack
+		{
+			if (simulate_stack.top() == pop_order[index_pop])
+			{
+				in_stack.erase(pop_order[index_pop]);
+				simulate_stack.pop();
+				++index_pop;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		// push until match pop_order
+		{
+			in_stack.insert(push_order[index_push]);
+			simulate_stack.push(push_order[index_push]);
+			++index_push;
+		}
+
+	}
 	return true;
 }
+
+bool isRightOrderText(vector<int> &push_order, vector<int> &pop_order)
+{
+	stack<int> simulate_stack;
+	int index_push = 0, index_pop = 0;
+
+	while (true)
+	{
+		if (index_pop == pop_order.size())
+			if (simulate_stack.empty())
+				return true;
+			else
+				return false;
+		if (!simulate_stack.empty() && simulate_stack.top() == pop_order[index_pop])
+		{
+			simulate_stack.pop();
+			++index_pop;
+		}
+		else
+		{
+			while (index_push < push_order.size() && (simulate_stack.empty() || simulate_stack.top() != pop_order[index_pop]))
+			{
+				simulate_stack.push(push_order[index_push]);
+				++index_push;
+			}
+			if (index_push == push_order.size() && simulate_stack.top() != pop_order[index_pop])
+				return false;
+		}
+	}
+}
+
+int main()
+{
+	vector<int> push_order({ 1, 2, 3, 4, 5 });
+	vector<int> pop_order({ 4,5,3,2,1 });
+
+	//cout << isRightOrder(push_order, pop_order) << endl;
+	cout << isRightOrderText(push_order, pop_order) << endl;
+	return 0;
+}
+ 
